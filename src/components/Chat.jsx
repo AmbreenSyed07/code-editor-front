fde5b3bb788de1df10e25e0680f27f29141e013e
AmbreenSyed07 <ambreen4724syed@gmail.com>
2025-02-02 01:00:24 +0530

    Replaced hard-coded url with environment variable.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import io from "socket.io-client";
 import PropTypes from "prop-types";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_API_URL);
 
 const Chat = ({ roomId }) => {
   const [message, setMessage] = useState("");
